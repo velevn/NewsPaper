8b450ce7d94e8304fcbdc1c122b83c039e8919ab
Nikita <velevnik@gmail.com>
2023-09-14 13:23:18 +0400

    просто

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'news',
     'accounts',
+    'django_filters',
 ]
 
 
--- news/views.py
+++ news/views.py
@@ -1,18 +1,24 @@
 from typing import Any
-from django.shortcuts import render
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
-from .models import Post 
-from datetime import datetime
+from .models import Post
+from .filters import PostFilter
+
 
 class PostList(ListView):
     model = Post
     template_name = 'news/news.html'
     context_object_name = 'posts_list'
+    paginate_by = 2
 
-    def get_context_data(self, **kwargs: Any):
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        context['next_public'] = None
+        context['filterset'] = self.filterset
         return context
 
 
@@ -20,8 +26,3 @@ class PostDetail(DetailView):
     model = Post
     template_name = 'news/single_news.html'
     context_object_name = 'post_list'
-
-
-
-# def news(request):
-#     return render(request, 'news/news.html')
